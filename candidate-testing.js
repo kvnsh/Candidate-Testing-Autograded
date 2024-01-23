e1701646ca207e46cf24e945063b85c6af72b4a2
kvnsh <kaveh.noshadi@gmail.com>
2024-01-23 00:02:54 -0600

    Candidate test

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,9 +6,10 @@ const input = require('readline-sync');
 
 let candidateName = input.question("What is your name?");
 console.log("Hello" + candidateName + "!");
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = input.question("Who was the first American woman in space? ");
-console.log(question);
+console.log (question);
 
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
@@ -21,23 +22,27 @@ if (correctAnswer === candidateAnswer){
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?","What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true","40","Trajectory","3"];
+let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("What is your name? ")
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-
+for (let i = 0; i < questions.length; i++){
+  let userAnswer = input.question(questions[i]);
+  candidateAnswers.push(userAnswer);
 }
 
-function gradeQuiz(candidateAnswers) {
 
+function gradeQuiz(candidateAnswers) {
+  console.log('Your responses : ${candidateAnswers} and correct responses : ${correctAnswers}');
+}
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
